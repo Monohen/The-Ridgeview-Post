58019417d72336b0df9a6a7c88b5f99027492f4b
Natalect <Natalect.org@protonmail.com>
2022-08-29 20:34:28 -0700

    Saving before I do something really destructive.

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import LoginBtn from "./login-btn";
 
 function Header() {
   return (
@@ -17,6 +18,7 @@ function Header() {
         >
           Ravens Report Channel
         </a>
+        <LoginBtn></LoginBtn>
       </div>
     </div>
   );
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,15 @@
-import "../styles/globals.css";
+import { SessionProvider } from "next-auth/react";
 import type { AppProps } from "next/app";
 import Header from "../components/Header";
+import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <div className="bg-neutral-50 min-h-screen">
-      <Header></Header>
-      <Component {...pageProps} />
+      <SessionProvider session={session}>
+        <Header></Header>
+        <Component {...pageProps} />
+      </SessionProvider>
     </div>
   );
 }
