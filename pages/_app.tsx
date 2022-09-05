58b56a1c32f617177234b0d2837ac4aa2b6788b3
Natalect <Natalect.org@protonmail.com>
2022-09-05 15:53:17 -0700

    Fixed the old script import to AdSense

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import Header from "../components/Header";
-import Head from "next/head";
+import Script from "next/script";
 
 export default function MyApp({
   Component,
@@ -10,6 +10,12 @@ export default function MyApp({
 }: AppProps) {
   return (
     <div className="bg-neutral-50 min-h-screen">
+      <Script
+        async
+        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6110297073296255"
+        crossOrigin="anonymous"
+      ></Script>
+
       <SessionProvider session={session}>
         <Header></Header>
         <Component {...pageProps} />
